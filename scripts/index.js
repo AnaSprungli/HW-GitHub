aae9ec352ca703518634ca37bb04a0a2bcde1973
Gonzalo Contreras <gonzalocontreras004@gmail.com>
2024-03-19 23:12:27 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if (Number.isInteger(numero)) {
+        if (numero % 2 === 0) {  
+            return numero.toString(2);
+        } else {
+            return numero.toString(16);
+        }
+    } else {
+        return "El parámetro no es un número entero.";
     }
+}
+console.log(laCajaDePandora(4));
+console.log(laCajaDePandora(37));
