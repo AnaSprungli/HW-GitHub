fd9e680e9ed6effd5a708b4bc691eaabf01218c7
Gonzalo Contreras <gonzalocontreras004@gmail.com>
2024-03-19 23:33:44 -0300

    new index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if (Number.isInteger(numero)) {
+        if (numero % 2 === 0) {  
+            return numero.toString(2);
+        } else {
+            return numero.toString(16);
+        }
+    } else {
+        return "El parámetro no es un número entero.";
     }
+}
+console.log(laCajaDePandora(4));
+console.log(laCajaDePandora(37));
+function datos(){
+    return{
+        nombre: "Gonzalo",
+        edad:"19 años",
+        nacionalidad: "Argentina",
+    };
+}
+console.log(datos());
